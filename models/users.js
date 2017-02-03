a2168e10a847e108df18b8529f70b74e36ac9716
Danielson <creativebitstudio@gmail.com>
2017-02-03 02:57:13 -0500

    files added to github

---
+++ models/users.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        unique: true,
+        requireed: true,
+        trim: true
+    },
+    
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    favoriteBook: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+});
+
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
